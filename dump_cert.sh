6c3c9ef1ef492be7df04025e1fe949b0daf20ee1
trodevel <trodevel@gmail.com>
2023-08-10 15:47:39 +0200

    intermediate results

--- dump_cert.sh
+++ dump_cert.sh
@@ -2,6 +2,6 @@
 
 CERT=$1
 
-[[ -z "$CERT" ]] && echo "ERROR: type $0 <certificate.cer>" && exit 1
+[[ -z "$CERT" ]] && echo "ERROR: type dump_cert.sh <certificate.cer>" && exit 1
 
 openssl x509 -text -noout -in "$CERT"
